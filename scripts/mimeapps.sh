1f6d3715d8cefa47b454a314ea96d458cc1bc009
lfchr <fclindell@gmail.com>
2026-07-12 21:23:25 +0200

    update

--- scripts/mimeapps.sh
+++ scripts/mimeapps.sh
@@ -3,7 +3,7 @@
 set -euxo pipefail
 
 rm /usr/share/applications/mimeapps.list
-curl -L https://gitlab.gnome.org/GNOME/gnome-session/-/raw/gnome-50/data/gnome-mimeapps.list -o /usr/share/applications/mimeapps.list
+cp /usr/share/applications/gnome-mimeapps.list /usr/share/applications/mimeapps.list
 
 sed -i 's/org.gnome.Builder.desktop/org.gnome.TextEditor.desktop/g' /usr/share/applications/mimeapps.list
 sed -i 's/org.gnome.Epiphany.desktop/org.mozilla.firefox.desktop/g' /usr/share/applications/mimeapps.list
