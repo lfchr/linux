5fa8ac0db737415b03f625616162864b2b713b74
lfchr <fclindell@gmail.com>
2026-07-17 21:24:41 +0200

    fix

--- scripts/finalize.sh
+++ scripts/finalize.sh
@@ -19,7 +19,7 @@ systemctl preset-all
 
 glib-compile-schemas /usr/share/glib-2.0/schemas
 
-sed -i "s|^EFIDIR=.*|EFIDIR=\"linux\"|" /usr/sbin/grub2-switch-to-blscfg
+sed -i "s|^EFIDIR=.*|EFIDIR=\"fedora\"|" /usr/sbin/grub2-switch-to-blscfg
 
 sed -i 's/timeout_style=menu/timeout_style=hidden/g' /usr/lib/bootupd/grub2-static/grub-static-pre.cfg
 
