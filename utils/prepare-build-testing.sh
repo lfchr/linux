4addf186aaea6f943fa51e2b43bcca5a5061f7a3
lfchr <fclindell@gmail.com>
2026-07-24 21:57:00 +0200

    fix locations

--- utils/prepare-build-testing.sh
+++ utils/prepare-build-testing.sh
@@ -3,18 +3,19 @@
 set -euxo pipefail
 
 files=$(realpath $(dirname $0)/../files)
-scripts=$(realpath $(dirname $0))
+scripts=$(realpath $(dirname $0)/../scripts)
+utils=$(realpath $(dirname $0))
 
 sed -i -e '/^dnf in/a\
 ${testing[@]} \\' \
-$scripts/rpm-packages.sh
+$scripts/packages/rpm.sh
 
 sed -i -e '/^OS_VERSION/ s/"$/ testing"/' \
     -e 's/stable/development/g' \
-$scripts/os-release.sh
+$utils/os-release.sh
 
 sed -i -e '/^groups=(/a\
     "testing"' \
-$scripts/flatpak.sh
+$scripts/packages/flatpak.sh
 
 rm -f $files/usr/lib/dracut/dracut.conf.d/30-intel.conf
