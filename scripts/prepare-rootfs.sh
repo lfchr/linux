d0caa95b0525450b4f2569de558c5639ffd80385
lfchr <fclindell@gmail.com>
2026-07-24 21:48:57 +0200

    remove comments

--- scripts/prepare-rootfs.sh
+++ scripts/prepare-rootfs.sh
@@ -8,8 +8,6 @@ scripts=$(realpath $(dirname $0))
 $scripts/packages/rpm-packages.sh
 $scripts/packages/flatpak.sh
 
-# prepare files
-
 cp -Rfv $files/* /
 
 $scripts/remove-fedora-branding.sh
@@ -17,8 +15,5 @@ $scripts/os-release.sh
 $scripts/mimeapps.sh
 $scripts/keyboard.sh
 
-# final steps
-
 $scripts/finalize.sh
-
 $scripts/rebuild-initrd.sh
